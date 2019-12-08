df92fb41aa4276e0bdc312162f7a1c904864cf64
Vergil Weatherford <vergilcw@gmail.com>
2019-12-07 21:45:52 -0500

    fix dockerfile

--- get-my-apps.sh
+++ get-my-apps.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#this file is set up to help get up and running on crostini. 
+#TODO: adapt this into a docker-based workflow, so it works anywhere docker does.
+
 sudo apt install -y gnupg2
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 
@@ -26,3 +30,17 @@ sudo apt install -y r-base-dev
 #Install gcloud SDK
 sudo apt-get install google-cloud-sdk
 
+#install latest docker (instructions from https://docs.docker.com/install/linux/docker-ce/debian/)
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+#make it so you don't have to run sudo for every docker command
+#(requires a full restart to take effect)
+sudo groupadd docker
+sudo usermod -aG docker vergilcw
+
+#install miniconda:
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
