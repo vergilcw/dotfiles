b645ce8e05fc3c75749e07215c6a8715567980ad
vergilcw <vergil.weatherford@guidehouse.com>
2024-08-09 02:18:45 +0000

    cleanup and git-prompt.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@
 
 # Download Git Auto-Completion
 curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > .git-completion.bash
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh" > .git-prompt.sh
+bash .git-prompt.sh
+
 
 dotdir="$(cd "$(dirname "$1")"; pwd)$(basename "$1")"
 homedir=$HOME
